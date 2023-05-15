6031e4046fb5023efb8d2479c3864ab694e4748e
Subhan Sohair <subhansohair@gmail.com>
2023-05-16 01:44:38 +0500

    Created Application Form & connected to DB

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_job_from_db
-
+from flask import Flask, render_template, jsonify, request 
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 
@@ -24,6 +23,16 @@ def show_job(id):
     if not JOB:
         return "Not Found", 404
     return render_template("jobpage.html", job=JOB)
+
+@app.route("/job/<id>/apply", methods = ['POST','GET'])
+def apply_to_job(id):
+    data = request.form
+    JOB = load_job_from_db(id)
+
+    add_application_to_db(id, data)
+
+    return render_template("application_submitted.html", application = data, job = JOB)
+    # return jsonify(data)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine, text
 import os
 
-# 
-# 
-# 
+#
+#
+#
 
 db_connection_string = os.environ['db_con_str']
 
@@ -16,21 +16,20 @@ engine = create_engine(
     }
 )
 
+
 col_name = ('id', 'title', 'location', 'salary',
-                'currency', 'responsibilties', 'requirements')
+            'currency', 'responsibilties', 'requirements')
+
 
 def load_jobs_from_db():
 
     with engine.connect() as conn:
         # col_names = conn.execute(text("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'jobs'"))
         result = conn.execute(text("select * from jobs"))
-
         result_all = result.all()
 
         jobs = []
-
         # list_dict.append(dict(zip(col_name,result_all[0])))
-
         for row in result_all:
             load_dict = dict(zip(col_name, row))
             jobs.append(load_dict)
@@ -42,13 +41,19 @@ def load_job_from_db(id):
 
     with engine.connect() as conn:
         result = conn.execute(text(f"SELECT * FROM jobs WHERE id = {id}"))
-            
-            
         rows = result.all()
         if len(rows) == 0:
             return None
         else:
             return dict(zip(col_name, rows[0]))
 
-
 # print(load_job_from_db(1))
+
+
+def add_application_to_db(job_id, data):
+
+    with engine.connect() as conn:
+
+        query = text(f"INSERT INTO applications (job_id, full_name, email, GitHub_URL, education, work_experience, resume_url) VALUES ('{job_id}', '{data['full_name']}', '{data['email']}', '{data['GitHub_URL']}', '{data['education']}', '{data['work_experience']}', '{data['resume_url']}')")
+
+        conn.execute(query)
